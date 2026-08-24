b1ed9384ce5fbe4b65ccdda976ed6d9a9db63758
Adriano <dantecristobalrios@gmail.com>
2026-08-24 02:12:17 -0500

    que flojera ctmr
    x

--- twrp_C29.mk
+++ twrp_C29.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 $(call inherit-product, device/miray/C29/device.mk)
 
@@ -21,6 +21,6 @@ PRODUCT_MANUFACTURER := miray
 PRODUCT_GMS_CLIENTID_BASE := android-miray
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="C29_C_EEA-user 12 UP1A.231005.007 1735015555 release-keys"
+    PRIVATE_BUILD_DESC="C29_C_EEA-user 12 UP1A.231005.007 1735015555 release-keys"
 
 BUILD_FINGERPRINT := CWOWDEFU/C29_C_EEA/C29:14/UP1A.231005.007/1735015555:user/release-keys
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,1 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo twrp_C29-user
-add_lunch_combo twrp_C29-userdebug
 add_lunch_combo twrp_C29-eng
