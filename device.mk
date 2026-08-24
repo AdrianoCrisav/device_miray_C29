f3925dea689a58a981ea562f99561e8bb912be95
Adriano <dantecristobalrios@gmail.com>
2026-08-24 01:54:56 -0500

    ptm me quede dormido en plena llamada
    ola

--- device.mk
+++ device.mk
@@ -19,11 +19,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.mt6765
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6765 \
-    libgptutils \
-    libz \
-    libcutils
+# adios hijo mio
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#     bootctrl.mt6765 \
+#     libgptutils \
+#     libz \
+#     libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
